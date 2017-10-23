89b4f4a841e7fbfe94de7a30a60320145abe06ee
Basar Agakadi <basaragakadi@gmail.com>
2017-10-23 21:42:07 +0300

    changed server.py in order to solve a problem on bluemix.

--- server.py
+++ server.py
@@ -1,46 +1,17 @@
 import datetime
 import os
-import json
-import re
-import psycopg2 as dbapi2
-import psycopg2
-from flask import redirect
+
 from flask import Flask
 from flask import render_template
 
 
 app = Flask(__name__)
 
-def get_elephantsql_dsn(vcap_services):
-    """Returns the data source name for ElephantSQL."""
-    parsed = json.loads(vcap_services)
-    uri = parsed["elephantsql"][0]["credentials"]["uri"]
-    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
-    user, password, host, _, port, dbname = match.groups()
-    dsn = """user='{}' password='{}' host='{}' port={}
-             dbname='{}'""".format(user, password, host, port, dbname)
-    return dsn
 
 @app.route('/')
 def home_page():
-    with dbapi2.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """DROP TABLE IF EXISTS USERS"""
-        cursor.execute(query)
-
-        query = """CREATE TABLE USERS(
-        ID SERIAL NOT NULL,
-        USERNAME VARCHAR(30),
-        PASSWORD VARCHAR(15),
-        MAIL_ID INTEGER,
-        POST_ID INTEGER,
-        PRIMARY KEY(ID)
-        )"""
-        cursor.execute(query)
-
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
-
 
 
 if __name__ == '__main__':
@@ -49,12 +20,4 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
-
-    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
-    if VCAP_SERVICES is not None:
-        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
-    else:
-        app.config['dsn'] = """user='vagrant' password='vagrant'
-                               host='localhost' port=5432 dbname='itucsdb'"""
-
     app.run(host='0.0.0.0', port=port, debug=debug)
