90ab399ca6ade6b52a66f026b177d061310e8bd4
Yahya Ceylani <Yahya Ceylani@192.168.1.27>
2017-12-07 02:52:54 +0300

    minor updates and initialize data for oscar table

--- initialize_db.py
+++ initialize_db.py
@@ -20,6 +20,13 @@ def initialize_db_function(cursor):
                     'USA')""")
 
     cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'NATALIE',
+                    'PORTMAN',
+                    'FEMALE',
+                    '1981-6-9',
+                    'ISRAEL')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
                     'JULIA',
                     'ROBERTS',
                     'FEMALE',
@@ -34,17 +41,149 @@ def initialize_db_function(cursor):
                     'UK')""")
 
     cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'BRIE',
+                    'LARSON',
+                    'FEMALE',
+                    '1989-10-1',
+                    'USA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'JULIANNE',
+                    'MOORE',
+                    'FEMALE',
+                    '1960-12-3',
+                    'USA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'CASEY',
+                    'AFFLECK',
+                    'MALE',
+                    '1975-8-12',
+                    'USA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'LEONARDO',
+                    'DICAPRIO',
+                    'MALE',
+                    '1974-11-11',
+                    'USA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'EDDIE',
+                    'REDMAYNE',
+                    'MALE',
+                    '1982-1-6',
+                    'UK')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'MATTHEW',
+                    'McCONAUGHEY',
+                    'MALE',
+                    '1969-11-4',
+                    'USA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
                     'EVA',
                     'GREEN',
                     'FEMALE',
                     '1980-7-6',
                     'FRANCE')""")
 
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'CATE',
+                    'BLANCHETT',
+                    'FEMALE',
+                    '1969-5-14',
+                    'AUSTRALIA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'DANIEL',
+                    'DAY-LEWIS',
+                    'MALE',
+                    '1957-4-29',
+                    'UK')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'JEAN',
+                    'DUJARDIN',
+                    'MALE',
+                    '1972-6-19',
+                    'FRANCE')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'COLIN',
+                    'FIRTH',
+                    'MALE',
+                    '1960-9-10',
+                    'UK')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'EMMA',
+                    'STONE',
+                    'FEMALE',
+                    '1988-11-6',
+                    'USA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'JENNIFER',
+                    'LAWRENCE',
+                    'FEMALE',
+                    '1990-8-15',
+                    'USA')""")
+
+    cursor.execute("""INSERT INTO ACTORS (NAME, SURNAME, GENDER, BIRTHDATE, COUNTRY) VALUES(
+                    'MERYL',
+                    'STREEP',
+                    'FEMALE',
+                    '1949-6-22',
+                    'USA')""")
+
     # Oscars table
-    cursor.execute("""DROP TABLE IF EXIST OSCARS CASCADE""")
+    cursor.execute("""DROP TABLE IF EXISTS OSCARS CASCADE""")
     cursor.execute("""CREATE TABLE OSCARS(
                     MOVIE VARCHAR(30),
                     ACTRESS VARCHAR(30),
                     ACTOR VARCHAR(30),
                     YEAR INT PRIMARY KEY NOT NULL)""")
 
+    cursor.execute("""INSERT INTO OSCARS (MOVIE, ACTRESS, ACTOR, YEAR) VALUES(
+                        'MOONLIGHT',
+                        'EMMA STONE',
+                        'CASEY AFFLECK',
+                        '2016')""")
+
+    cursor.execute("""INSERT INTO OSCARS (MOVIE, ACTRESS, ACTOR, YEAR) VALUES(
+                        'SPOTLIGHT',
+                        'BRIE LARSON',
+                        'LEONARDO DICAPRIO',
+                        '2015')""")
+
+    cursor.execute("""INSERT INTO OSCARS (MOVIE, ACTRESS, ACTOR, YEAR) VALUES(
+                        'BIRDMAN',
+                        'JULIANNE MOORE',
+                        'EDDIE REDMAYNE',
+                        '2014')""")
+
+    cursor.execute("""INSERT INTO OSCARS (MOVIE, ACTRESS, ACTOR, YEAR) VALUES(
+                        '12 YEARS A SLAVE',
+                        'CATE BLANCHETT',
+                        'MATTHEW McCONAUGHEY',
+                        '2013')""")
+
+    cursor.execute("""INSERT INTO OSCARS (MOVIE, ACTRESS, ACTOR, YEAR) VALUES(
+                        'ARGO',
+                        'JENNIFER LAWRANCE',
+                        'DANIEL DAY-LEWIS',
+                        '2012')""")
+
+    cursor.execute("""INSERT INTO OSCARS (MOVIE, ACTRESS, ACTOR, YEAR) VALUES(
+                        'THE ARTIST',
+                        'MERYL STREEP',
+                        'JEAN DUJARDIN',
+                        '2011')""")
+
+    cursor.execute("""INSERT INTO OSCARS (MOVIE, ACTRESS, ACTOR, YEAR) VALUES(
+                        'THE KING''S SPEECH',
+                        'NATALIE PORTMAN',
+                        'COLIN FIRTH',
+                        '2010')""")
